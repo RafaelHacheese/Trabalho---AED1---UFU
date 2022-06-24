a0869c4c29b2e75c2624f72744fe3ed755be4bef
Vitor Yuji <83984086+VitorYuji25@users.noreply.github.com>
2022-06-24 08:18:07 -0300

    Update lista.h
    Signed-off-by: Vitor Yuji <83984086+VitorYuji25@users.noreply.github.com>

--- 01_Lista_String/lista.h
+++ 01_Lista_String/lista.h
@@ -7,11 +7,10 @@ Lista cria_lista();
 char getPos(Lista lst, int pos);
 int lista_vazia(Lista lst);
 int lista_cheia(Lista lst);
-int inserir_elem(Lista lst, char str[]);
 int insere_ord(Lista lst, char str[]);
 int remove_elem(Lista lst, char str[]);
 int tamanho_lista(Lista lst);
 int esvazia_lista(Lista lst);
 int remove_todas(Lista lst, char elem[]);
-
+Lista intercala_listas(Lista ,Lista );
 #endif // LISTA_H_INCLUDED
